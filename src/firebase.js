4ffd1aa3e8f7156de204a8312b4833190be17c97
ZohebS-dxb <zubisid@gmail.com>
2025-06-17 03:20:05 +0400

    test

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyClffvlLY66cKD6ex6eOygg8uS9rbKvCns",
+ apiKey: "AIzaSyClffvlLY66cKD6ex6eOygg8uS9rbKvCns",
   authDomain: "liar-1-ebc12.firebaseapp.com",
   projectId: "liar-1-ebc12",
   storageBucket: "liar-1-ebc12.firebasestorage.app",
@@ -13,5 +11,7 @@ const firebaseConfig = {
   appId: "1:853929867266:web:12fd05aa5cfdc0746d0f1b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
