718943f4f8ff55ebea188b9d6527182f4288918c
ZohebS-dxb <zubisid@gmail.com>
2025-06-17 06:13:26 +0400

    d

--- src/questions.js
+++ src/questions.js
@@ -1,8 +1,1 @@
-const questions = [
-  "How many children do you want?",
-  "What’s your ideal weekend activity?",
-  "Which animal would you be in another life?",
-  "What's your favorite snack at the movies?"
-];
-
-export default questions;
+...
